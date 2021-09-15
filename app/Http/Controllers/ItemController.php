e22db0359f5d7e0651443b2df2eccbfbb3ab8ddc
dinushchathurya <dinushchathurya21@gmail.com>
2021-09-15 21:44:11 +0530

    :hammer: Refactor functions

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -7,28 +7,28 @@ use App\Models\Item;
 
 class ItemController extends Controller
 {
-    public function index()
+   public function index()
     {
-        $pendingItem = Item::where('status', 0)->orderBy('order')->get();
-        $completeItem = Item::where('status', 1)->orderBy('order')->get();
+    	$pendingItem = Item::where('status',0)->orderBy('order')->get();
+    	$completeItem = Item::where('status',1)->orderBy('order')->get();
 
-        return view('welcome', compact('pendingItem', 'completeIte'));
+    	return view('welcome',compact('pendingItem','completeItem'));
     }
 
-    public function updateItem()
+    public function updateItems(Request $request)
     {
-        $input = $request->all();
+    	$input = $request->all();
 
-        foreach ($input['pendingArr'] as $key => $value) {
-            $key = $key + 1;
-            Item::where('id', $value)->update(['status'=> 0 , 'order'=> $key]);
-        }
+    	foreach ($input['pendingArr'] as $key => $value) {
+    		$key = $key+1;
+    		Item::where('id',$value)->update(['status'=>0,'order'=>$key]);
+    	}
 
-        foreach ($input['completeArr'] as $key => $value) {
-            $key = $key + 1;
-            Item::where('id', $value)->update(['status'=> 1 , 'order'=> $key]);
-        }
+    	foreach ($input['completeArr'] as $key => $value) {
+    		$key = $key+1;
+    		Item::where('id',$value)->update(['status'=>1,'order'=>$key]);
+    	}
 
-        return response()->json(['status'=>'success']);
+    	return response()->json(['status'=>'success']);
     }
 }
